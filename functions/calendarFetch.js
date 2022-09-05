010ad57438308de52348e865d234dc6eb8daae16
Nick Dalby <nckdlby@gmail.com>
2022-09-05 13:56:24 +0200

    hello world

--- functions/calendarFetch.js
+++ functions/calendarFetch.js
@@ -13,30 +13,37 @@ const headers = {
   'Access-Control-Allow-Credentials': 'true',
 }
 
-export const handler = async (event, context) => {
-  const finalUrl = `${BASEURL}&key=${VITE_GOOGLE_CALENDAR_API_KEY}${
-    event.queryStringParameters.maxResults
-      ? `&maxResults=${event.queryStringParameters.maxResults}`
-      : ''
-  }`
-  try {
-    if (event.httpMethod === 'GET') {
-      return fetch(finalUrl)
-        .then((response) => response.json())
-        .then((data) => ({
-          statusCode: 200,
-          body: JSON.stringify(data.items, null, 2),
-          headers: JSON.stringify(headers),
-        }))
-    }
-    return {
-      statusCode: 401,
-    }
-  } catch (error) {
-    console.log(error)
-    return {
-      statusCode: 500,
-      body: error.toString(),
-    }
+// export const handler = async (event, context) => {
+//   const finalUrl = `${BASEURL}&key=${VITE_GOOGLE_CALENDAR_API_KEY}${
+//     event.queryStringParameters.maxResults
+//       ? `&maxResults=${event.queryStringParameters.maxResults}`
+//       : ''
+//   }`
+//   try {
+//     if (event.httpMethod === 'GET') {
+//       return fetch(finalUrl)
+//         .then((response) => response.json())
+//         .then((data) => ({
+//           statusCode: 200,
+//           body: JSON.stringify(data.items, null, 2),
+//           headers: JSON.stringify(headers),
+//         }))
+//     }
+//     return {
+//       statusCode: 401,
+//     }
+//   } catch (error) {
+//     console.log(error)
+//     return {
+//       statusCode: 500,
+//       body: error.toString(),
+//     }
+//   }
+// }
+
+export const handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
   }
 }
